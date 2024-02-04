9254ae95064590536bdc09e2991fcca5223ca595
KhingZaw <139222945+KhingZaw@users.noreply.github.com>
2024-02-04 17:31:13 +0630

    add CoffeShop

---
+++ CoffeShop/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeShop;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ CoffeShop/obj/Debug/net7.0-android/android/src/crc64d097a3b1d9c08a5c/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64d097a3b1d9c08a5c;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("CoffeShop.MainActivity, CoffeShop", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CoffeShop.MainActivity, CoffeShop", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CoffeShop.MainActivity, CoffeShop", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
